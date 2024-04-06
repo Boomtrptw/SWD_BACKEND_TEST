00952d1557010b07c9d1e71bb492cd8575b72df3
Boom <boommeekung44@gmail.com>
2024-04-06 12:08:23 +0700

    [Edited]: test1.2-1.3

--- todo/test1_3.py
+++ todo/test1_3.py
@@ -1,3 +1,8 @@
+"""
+เขียนโปรแกรมหาจำนวนเลข 0 ที่อยู่ติดกันหลังสุดของค่า factorial ด้วย Python โดยห้ามใช้ math.factorial 
+เช่น 7! = 5040 มีเลข 0 ต่อท้าย 1 ตัว, 10! = 3628800 มีเลข 0 ต่อท้าย 2 ตัว
+"""
+
 def find_trailing_zeros_in_factorial(n):
     # คำนวณ factorial ของจำนวน n
     factorial_result = 1
